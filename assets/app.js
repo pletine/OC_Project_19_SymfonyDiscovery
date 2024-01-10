d70d7aa9c5e8d265cf270564cf419d94c75d11fa
pletine <pierre.letinevez@gmail.com>
2024-01-10 14:01:55 +0400

    Show the form to add a comment when the user client on the title "Add comment"

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,21 @@
 import './styles/app.css'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+// Add click event on each h3 Comment Add
+var h3_addComment = document.getElementsByClassName('add comments');
+
+Array.from(h3_addComment).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        let nextSibling = e.target.nextElementSibling;
+        console.log(nextSibling.style.visibility);
+
+        if( nextSibling.style.visibility == 'visible' ) {
+            nextSibling.style.visibility = 'hidden';
+            nextSibling.style.height = '0';
+        } else {
+            nextSibling.style.visibility = 'visible';
+            nextSibling.style.height = 'auto';
+        }
+    });
+});
